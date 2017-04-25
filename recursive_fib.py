b61f2621b046d92e3351b3de23c3f209b5b00981
Philip Carter <pc@ucf.edu>
2017-04-25 10:23:13 -0400

    Added memozation cache to recurisive function.

--- recursive_fib.py
+++ recursive_fib.py
@@ -1,13 +1,20 @@
 "Calculates the nth Fibonacci number."
 
-n = 12
+# Create a memoization cache so our recursion is faster.
+fib_cache = {}
 
 def fib(n):
+    if n in fib_cache:
+        return fib_cache[n]
     if n == 0:
         return 0
     elif n == 1:
         return 1
+    elif n == 2:
+        return 1
     else:
+        fib_cache[n] = fib(n-1) + fib(n-2)
         return fib(n-1) + fib(n-2)
 
-print fib(n)
+for n in range(1, 501):
+    print "%s : %s" % (n, fib(n))
